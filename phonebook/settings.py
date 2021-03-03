0b7547bbd19ba7f030079db378aa8c6bcf404f5e
Altynbek1234 <altyw.16@gmail.com>
2021-03-03 14:16:15 -0500

    initial commit

--- phonebook/settings.py
+++ phonebook/settings.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
+
 """
 import os
 from pathlib import Path
 
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -117,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+django_heroku.settings(locals())
